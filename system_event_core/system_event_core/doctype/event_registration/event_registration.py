23c248130d8f2eca02204f30c1bdbd5dda4a77c9
Roshan Kumar B <118297543+Roshankumarb31@users.noreply.github.com>
2026-05-06 12:28:03 +0530

    Event Registration doctype completed

--- system_event_core/system_event_core/doctype/event_registration/event_registration.py
+++ system_event_core/system_event_core/doctype/event_registration/event_registration.py
@@ -14,7 +14,31 @@ class EventRegistration(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		hi: DF.Data | None
+		address: DF.SmallText | None
+		age: DF.Int
+		approval_date: DF.Date | None
+		approved_by: DF.SmallText | None
+		date_of_birth: DF.Date | None
+		email: DF.Data
+		emergency_contact_name: DF.Data | None
+		emergency_contact_phone: DF.Data | None
+		event: DF.SmallText
+		fee_amount: DF.Currency
+		full_name: DF.Data
+		gender: DF.Literal["Male", "Female"]
+		member: DF.SmallText | None
+		naming_series: DF.Literal["REG-.YYYY.-"]
+		number_of_attendees: DF.Int
+		payment_reference: DF.SmallText | None
+		payment_status: DF.Literal["Unpaid", "Partially Paid", "Paid", "Waived"]
+		phone: DF.Data
+		registered_by: DF.SmallText
+		registration_date: DF.Date
+		registration_id: DF.Data | None
+		registration_status: DF.Literal["Pending", "Confirmed", "Cancelled", "Wait listed", "Attended"]
+		registration_type: DF.Literal["Individual", "Family", "Group"]
+		remarks: DF.Text | None
+		special_requirements: DF.SmallText | None
 	# end: auto-generated types
 
 	pass
